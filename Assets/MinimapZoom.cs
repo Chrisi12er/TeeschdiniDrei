0588a3dc7e11f401743c8c1af83cbf06bb74f457
ReneSteindl <rene.steindl17@gmail.com>
2018-01-23 16:27:19 +0100

    moin leute

---
+++ Assets/MinimapZoom.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapZoom : MonoBehaviour
+{
+
+    Camera minimapCamera;
+    int currentZoom;
+    public int zoomMax;
+    public int[] zoomArray;
+
+	void Start ()
+    {
+        minimapCamera = this.GetComponent<Camera>();
+        currentZoom = 2;
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown("j"))
+        {
+            currentZoom -= 1;
+        }
+        if (Input.GetKeyDown("k"))
+        {
+            currentZoom += 1;
+        }
+
+        if (currentZoom > zoomMax)
+        {
+            currentZoom = zoomMax;
+        }
+        else if (currentZoom < 1)
+        {
+            currentZoom = 1;
+        }
+
+        minimapCamera.orthographicSize = zoomArray[currentZoom - 1];
+
+    }
+}
